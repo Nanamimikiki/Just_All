ac4c7181f57c0adb04941b7015d9716432ee3dfc
Evgeny Sablya <nam.prog.acc@gmail.com>
2023-04-03 20:57:52 +0300

    Reshil zadachku

--- segments/Main.java
+++ segments/Main.java
@@ -5,21 +5,9 @@ import java.util.ArrayList;
 public class Main {
     public static void main(String[] args) {
         ArrayList<Segment> segments = new ArrayList<>();
-        segments.add(new Segment(new Dot(), new Dot()));
-        segments.add(new Segment(new Dot(), new Dot()));
-        segments.add(new Segment(new Dot(), new Dot()));
-        segments.add(new Segment(new Dot(), new Dot()));
-        segments.add(new Segment(new Dot(), new Dot()));
-        segments.add(new Segment(new Dot(), new Dot()));
-        segments.add(new Segment(new Dot(), new Dot()));
-        segments.add(new Segment(new Dot(), new Dot()));
-        segments.add(new Segment(new Dot(), new Dot()));
-        segments.add(new Segment(new Dot(), new Dot()));
-        segments.add(new Segment(new Dot(), new Dot()));
-        segments.add(new Segment(new Dot(), new Dot()));
-        segments.add(new Segment(new Dot(), new Dot()));
-        segments.add(new Segment(new Dot(), new Dot()));
-        segments.add(new Segment(new Dot(), new Dot()));
+        for (int i = 10; i > 0; i--){
+            segments.add(new Segment(new Dot(), new Dot()));
+        }
         Double temp = 0.0;
         for (Segment segment: segments) {
             Double length = segment.getLength();
