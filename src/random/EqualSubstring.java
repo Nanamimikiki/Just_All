0bca4ea43918479a2efd49f4a95278e4638e3baa
Nanamimikiki <nam.prog.acc@gmail.com>
2024-05-28 14:46:25 +0300

    Just ALl

--- src/random/EqualSubstring.java
+++ src/random/EqualSubstring.java
@@ -2,6 +2,13 @@ package random;
 
 public class EqualSubstring {
     public int equalSubstring(String s, String t, int maxCost) {
-
+        int j = 0;
+        for (int i = 0; i < s.length(); i++) {
+            maxCost -= Math.abs(s.charAt(i) - t.charAt(i));
+            if (maxCost < 0) {
+                maxCost += Math.abs(s.charAt(j) - t.charAt(j++));
+            }
+        }
+        return s.length() - j;
     }
 }
